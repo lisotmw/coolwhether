ede19fe17ef87729b0c40b5fc8c7d83545a696eb
lisotmw <zhuo17609585015@gmail.com>
2017-12-11 21:23:45 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/example/just_being/coolweather/db/Province.java
@@ -0,0 +1,38 @@
+package com.example.just_being.coolweather.db;
+
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by Just_Being on 2017/12/11.
+ */
+
+public class Province extends DataSupport {
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
